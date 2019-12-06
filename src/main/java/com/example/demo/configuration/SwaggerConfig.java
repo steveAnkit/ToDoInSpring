b57407c8ce0caf50ea186be42ecf4723702a6860
steveAnkit <ankitsharma1702@gmail.com>
2019-12-06 13:07:18 +0530

    swagger configuration changes

--- src/main/java/com/example/demo/configuration/SwaggerConfig.java
+++ src/main/java/com/example/demo/configuration/SwaggerConfig.java
@@ -1,16 +1,10 @@
 package com.example.demo.configuration;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
-import java.util.ArrayList;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,33 +13,37 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	private static final String CONTROLLER_BASE_PATH = "com.example.demo.controller";
+	
 	@Bean
 	public Docket authorApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
-				.paths(regex("/api/v1.*"))
-				.build()
-				.apiInfo(metaInfo());
+				.apis(RequestHandlerSelectors.basePackage(CONTROLLER_BASE_PATH))
+				.paths(PathSelectors.any())
+				.build();
 				
 				
 	}
 	
-	private ApiInfo metaInfo() {
-
-		ApiInfo apiInfo = new ApiInfo(  "TO Spring Boot Application",
-		        "this application is for managing the tasks",
-		        "1.0",
-		        "Terms of Service",
-		        new Contact("Ankit Sharma", "https://www.linkedin.com/in/ankit-sharma-90446755/", "ankitsharma1702@gmail.com"),
-		        "Apache License Version 2.0",
-		        "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
-        return apiInfo;
 	
-        
-      
-
-}
+	// Un-comment for the additional meta info 
+	
+	/*
+	 * private ApiInfo metaInfo() {
+	 * 
+	 * ApiInfo apiInfo = new ApiInfo( "TO Spring Boot Application",
+	 * "this application is for managing the tasks", "1.0", "Terms of Service", new
+	 * Contact("Ankit Sharma", "https://www.linkedin.com/in/ankit-sharma-90446755/",
+	 * "ankitsharma1702@gmail.com"), "Apache License Version 2.0",
+	 * "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>());
+	 * return apiInfo;
+	 * 
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 	
 	
 	
